475137fab3bf4b1ee6120a635c66e0e60eac3704
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2024-03-28 23:10:40 +0530

    Feat: Image compress done

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from werkzeug.utils import secure_filename
 import os
 import subprocess
 from pdf2docx import Converter
+from PIL import Image
+
 
 def compress_pdf(input_path, output_path, quality='ebook'):
     """
@@ -30,11 +32,30 @@ def compress_pdf(input_path, output_path, quality='ebook'):
     subprocess.run(args)
     return "file compressed successfully"
 
+
 def convertpdftodoc(pdf_file, docx_file):
     cv = Converter(pdf_file)
     cv.convert(docx_file)  # all pages by default
     cv.close()
     return "file converted successfully"
+
+
+def compress_image(input_path, output_path, quality=85):
+    """
+    Compresses an image and saves it to the output path.
+
+    Parameters:
+        input_path (str): Path to the input image file.
+        output_path (str): Path to save the compressed image.
+        quality (int): The quality of the compressed image (0-100).
+    """
+    try:
+        with Image.open(input_path) as img:
+            img.save(output_path, optimize=True, quality=quality)
+        return "image compressed successfully"
+    except Exception as e:
+        print(f"Error compressing image: {e}")
+
 
 app = Flask(__name__)
 
@@ -96,6 +117,22 @@ def convert_to_doc():
     print(output_file_path)
     file.save(input_file_path)
     message = convertpdftodoc(input_file_path,output_file_path)
+    return message
+
+@app.route("/compressimage",methods=['POST'])
+def reduceImageSize():
+    if 'file' not in req.files:
+        return "No files uploaded", 400
+    file = req.files['file']
+    if file.filename == '':
+        return "No file selected", 400
+    input_file_path = os.path.join("uploads", secure_filename(file.filename))
+    output_file_path = os.path.join("uploads", "compressed_" + secure_filename(file.filename))
+    file.save(input_file_path)
+
+    quality = req.form.get('quality', 85)
+    print("quality is ", quality)
+    message = compress_image(input_file_path, output_file_path, quality)
     return message
 
 
