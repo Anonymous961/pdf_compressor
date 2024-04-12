032fd889fb73fa67a77758bebcccd61bfabe60e1
ANIL KUMAR BEHERA <ark845612@gmail.com>
2024-04-13 00:12:13 +0530

    test:everything working fine

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ def compress_image(input_path, output_path, quality=85):
         print(f"Error compressing image: {e}")
 
 
-
 app = Flask(__name__)
 
 
@@ -75,7 +74,7 @@ def show_username(username):
         return "not proper method"
 
 
-#check file upload
+# check file upload
 @app.route("/upload", methods=["POST"])
 def upload():
     if 'file' not in req.files:
@@ -88,7 +87,7 @@ def upload():
     return "File saved successfully!"
 
 
-#to compress file
+# to compress file
 @app.route("/compresspdf", methods=['POST'])
 def upload_file():
     if 'file' not in req.files:
@@ -110,7 +109,7 @@ def upload_file():
     return jsonify(data)
 
 
-@app.route('/converttodoc',methods=["POST"])
+@app.route('/converttodoc', methods=["POST"])
 def convert_to_doc():
     if 'file' not in req.files:
         return "No files uploaded", 400
@@ -120,38 +119,41 @@ def convert_to_doc():
     input_file_path = os.path.join("uploads", secure_filename(file.filename))
     # print(file.filename)
     filename, _ = os.path.splitext(secure_filename(file.filename))
-    output_file_path = os.path.join("uploads", "doc_" + filename + ".doc")
+    output_file = "doc_" + filename + ".doc"
+    output_file_path = os.path.join("uploads", output_file)
     print(output_file_path)
     file.save(input_file_path)
-    message = convertpdftodoc(input_file_path,output_file_path)
-    return message
+    message = convertpdftodoc(input_file_path, output_file_path)
+    data = {"output_file": output_file, "message": message}
+    return jsonify(data)
 
-@app.route("/compressimage",methods=['POST'])
-def reduceImageSize():
+
+@app.route("/compressimage", methods=['POST'])
+def reduceimagesize():
     if 'file' not in req.files:
         return "No files uploaded", 400
     file = req.files['file']
     if file.filename == '':
         return "No file selected", 400
     input_file_path = os.path.join("uploads", secure_filename(file.filename))
-    output_file_path = os.path.join("uploads", "compressed_" + secure_filename(file.filename))
+    output_file = "compressed_" + secure_filename(file.filename)
+    output_file_path = os.path.join("uploads", output_file)
     file.save(input_file_path)
 
-    quality = req.form.get('quality', 85)
+    quality = int(req.form.get('quality', 50))
     print("quality is ", quality)
     message = compress_image(input_file_path, output_file_path, quality)
-    return message
+    data = {"output_file": output_file, "message": message}
+    return jsonify(data)
 
 
-
-#to get any file
+# to get any file
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
     try:
         return send_file(os.path.join("uploads", filename), as_attachment=True)
     except FileNotFoundError:
         return "File not found", 404
-
 
 
 if __name__ == '__main__':
